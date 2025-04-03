3d2d2561ac2a2937d7668a1a179332dc4318e6a9
CarlosBF <cborrellf@outlook.es>
2025-04-03 22:03:46 +0200

    Deleted localStorage usage. Reimplemented addEventListener to country cards

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 const countryList = document.getElementById('countries-list')
 const floatingWindow = document.getElementById('floating-window')
-let countryId = -1
 
 //Template of a div with id, name and flag
 const template = (country) => {
-    countryId++
-    localStorage.setItem(countryId, JSON.stringify(country))
     return `
-        <div class="country" id="${countryId}" onclick="popUp(${countryId})">
+        <div class="country">
             <img src = ${country.flags[1]}>
             <h4>${country.name.common}</h4>
         </div>
@@ -21,51 +18,45 @@ const getCountries = async () => {
             throw new Error('Error: '+response.status)
         }
         //Get countries
-        let countries = await response.json()
+        const countries = await response.json()
         //Sort countries
         countries.sort((a, b) => a.name.common > b.name.common)
         //Create divs
-        countries = countries.map((country) => template(country))
-        //Save divs as string
-        localStorage.setItem('countries', countries.join(""))
+        let ans = countries.map((country) => template(country))
         //Update HTML
-        countryList.innerHTML = countries.join("")
+        countryList.innerHTML = ans.join("")
+        //Add Event Listeners to the countries
+        const cards = document.querySelectorAll('.country')
+        cards.forEach((card, index) => {
+            card.addEventListener('click', () => {
+                const data = countries[index]
+                floatingWindow.style.display = "block"
+                floatingWindow.innerHTML = `
+                    <div class="floatingContent">
+                        <div>
+                            <img src="${data.flags[1]}">
+                            <div class="info">
+                                <h2>${data.name.common}</h2>
+                                <p>Capital: ${data.capital ? data.capital[0] : 'No tiene'}</p>
+                                <p>Población: ${data.population}</p>
+                                <p>Lado de la carretera: ${data.car.side}</p>
+                            </div>
+                        </div>
+                        <button onclick="closeInfo()">Cerrar</button>
+                    </div>
+                `
+            })
+        })
+
     }catch(err){
         console.error(err)
     }
 }
 
-//Check for countries in localStorage
-if(localStorage.getItem('countries') == null){
-    getCountries()
-}else{
-    countryList.innerHTML = localStorage.getItem('countries')
-}
-
-//Creates a popUp window with info of the selected country
-function popUp(Id) {
-    let data = JSON.parse(localStorage.getItem(Id))
-    console.log(data)
-    floatingWindow.style.display = "block"
-    floatingWindow.innerHTML = `
-        <div class="floatingContent">
-            <div>
-                <img src="${data.flags[1]}">
-                <div class="info">
-                    <h2>${data.name.common}</h2>
-                    <p>Capital: ${data.capital ? data.capital[0] : 'No tiene'}</p>
-                    <p>Población: ${data.population}</p>
-                    <p>Lado de la carretera: ${data.car.side}</p>
-                </div>
-            </div>
-            <button id="closeBtn">Cerrar</button>
-        </div>
-    `
-    document.getElementById('closeBtn').addEventListener('click', close)
-}
+getCountries()
 
 //Closes the popUp window
-function close() {
-    floatingWindow.style.display = "none"
+function closeInfo(){
+    floatingWindow.style.display = 'none'
     floatingWindow.innerHTML = ''
 }
---
+++ js/scriptLiveReview.js
@@ -0,0 +1,69 @@
+const apiEndPoint = "https://restcountries.com/v3/all"
+const countriesList = document.getElementById("countries-list")
+const info = document.getElementById("info")
+
+const getCountries = async () => {
+    try {
+    const response = await fetch(apiEndPoint)
+    if(!response.ok) {
+        throw new Error(`Error: ${response.status}`)
+    }
+    const countries = await response.json()
+    sortedCountries(countries)
+    countriesList.innerHTML = countryArray(countries).join("")
+    
+    const cards = document.querySelectorAll(".card")
+
+    cards.forEach((card, index) => {
+      card.addEventListener("click", () => {
+        const country = countries[index]
+        const { name: {common}, flags } = country
+        const template = `
+          <div class="cardInfo">
+            <img src=${flags[1]} alt=${common}/>
+            <h2>${common}</h2>
+            <button onclick="closeInfo()">Cerrar</button>
+          </div>
+        `
+        info.innerHTML = template
+        info.classList.add("visible")
+      })
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+getCountries()
+
+
+const closeInfo = () => {
+  info.innerHTML = ""
+  info.classList.remove("visible")
+}
+
+const templateCard = (name, flag) => {
+  const template = `
+    <div class="card">
+    <img src=${flag[1]} alt=${name}/>
+    <h2>${name}</h2>
+    </div>
+    `
+  return template
+}
+
+const countryArray = (countries) => {
+  const allCountries = countries.map(country => {
+    const { name: {common}, flags } = country
+    return templateCard(common, flags)
+  })
+  return allCountries
+} 
+
+const sortedCountries = (countries) => {
+  countries.sort((a,b) => {
+    const nameA = a.name.common.toUpperCase()
+    const nameB = b.name.common.toUpperCase()
+    return nameA.localeCompare(nameB)
+  })
+}
